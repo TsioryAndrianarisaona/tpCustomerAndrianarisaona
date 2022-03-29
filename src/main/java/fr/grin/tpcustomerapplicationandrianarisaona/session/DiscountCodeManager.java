d6052e5b69a6fb6683e830b5afce74aa65868e67
Tsiory Andrianarisaona <tsiory.andrianarisaona@hotmail.com>
2022-03-29 17:09:32 +0300

    Show details on customer

---
+++ src/main/java/fr/grin/tpcustomerapplicationandrianarisaona/session/DiscountCodeManager.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package fr.grin.tpcustomerapplicationandrianarisaona.session;
+
+import fr.grin.tpcustomerapplicationandrianarisaona.entities.DiscountCode;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Andrianarisaona
+ */
+@Stateless
+public class DiscountCodeManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+ /**
+  * 
+  * @param discountCode 
+  */
+    public void persist(DiscountCode discountCode) {
+        try {
+            em.persist(discountCode);
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    public List<DiscountCode> getAllDiscountCodes() {
+        Query query = em.createNamedQuery("DiscountCode.findAll");
+        return query.getResultList();
+    }
+
+    public DiscountCode findById(String discountCode) {
+        return em.find(DiscountCode.class, discountCode);
+    }
+}
